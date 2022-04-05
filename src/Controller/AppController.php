7636d49dd184923ecb792dd5d8f4b851af6735c1
Permata @ Azlan <permata@tyrellsys.com.my>
2022-04-05 11:39:23 +0800

    bin/cake bake plugin PermataTyrell/ArmyKnife

---
+++ src/Controller/AppController.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace PermataTyrell\ArmyKnife\Controller;
+
+use App\Controller\AppController as BaseController;
+
+class AppController extends BaseController
+{
+}
